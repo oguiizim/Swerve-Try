ed62ae8834d9bf10c0929fc202769ad110e5e0c7
Guilherme-Almeida <guilhermecordeiroyt@gmail.com>
2024-09-21 22:57:02 -0300

    test for locking angle

--- Swerve-Test/src/main/java/frc/robot/RobotContainer.java
+++ Swerve-Test/src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -18,38 +19,29 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.Controle;
 import frc.robot.commands.DriveCommand;
-import frc.robot.commands.SlowDrive;
 import frc.robot.subsystems.Swerve;
 
 public class RobotContainer {
 
   public static final Swerve swerve = new Swerve(new File(Filesystem.getDeployDirectory(), "swerve"));
+  public static final XboxController control = new XboxController(0);
   public static final CommandXboxController driver = new CommandXboxController(0);
 
   private final SendableChooser<Command> autoChooser;
-  private double maxSpeed = 4.0;
 
   public RobotContainer() {
-
-    // Command slow = new SlowDrive(swerve,
-    // () -> -MathUtil.applyDeadband(driver.getLeftX(), Controle.DEADBAND),
-    // () -> -MathUtil.applyDeadband(driver.getLeftY(), Controle.DEADBAND),
-    // () -> -MathUtil.applyDeadband(driver.getRawAxis(2), Controle.DEADBAND));
 
     swerve.setDefaultCommand(new DriveCommand(swerve,
         () -> -MathUtil.applyDeadband(driver.getLeftX(), Controle.DEADBAND),
         () -> -MathUtil.applyDeadband(driver.getLeftY(), Controle.DEADBAND),
-        () -> -MathUtil.applyDeadband(driver.getRawAxis(2), Controle.DEADBAND)
-        // maxSpeed
-        ));
+        () -> -MathUtil.applyDeadband(driver.getRawAxis(2), Controle.DEADBAND),
+        control));
 
     Command driveSim = swerve.simDriveCommand(() -> MathUtil.applyDeadband(driver.getLeftX(), Controle.DEADBAND),
         () -> MathUtil.applyDeadband(driver.getLeftY(), Controle.DEADBAND), () -> driver.getRawAxis(2));
 
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Chooser", autoChooser);
-
-    // swerve.setDefaultCommand(!RobotBase.isSimulation() ? drive : driveSim);
 
     configureBindings();
   }
@@ -58,17 +50,10 @@ public class RobotContainer {
     Trigger rightTrigger = driver.button(3);
 
     driver.button(2).onTrue(Commands.runOnce(swerve::zeroGyro));
-    new Trigger(rightTrigger).whileTrue(Commands.run(() -> setMax()));
   }
 
   public Command getAutonomousCommand() {
     return autoChooser.getSelected();
-  }
-
-  private void setMax() {
-    if (maxSpeed != 1.5) {
-      maxSpeed = 1.5;
-    }
   }
 
   public void setHeadingCorrection() {
--- Swerve-Test/src/main/java/frc/robot/commands/DriveCommand.java
+++ Swerve-Test/src/main/java/frc/robot/commands/DriveCommand.java
@@ -3,12 +3,12 @@ package frc.robot.commands;
 import java.util.List;
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.Dimensoes;
 import frc.robot.subsystems.Swerve;
 import swervelib.SwerveController;
@@ -20,16 +20,16 @@ public class DriveCommand extends Command {
      private boolean initRotation = false;
      private SwerveController controller;
      double velocity;
+     private PIDController pid = new PIDController(1.5, 0, 0);
 
-     XboxController control = new XboxController(0);
+     XboxController control;
 
-     public DriveCommand(Swerve swerve, DoubleSupplier vY, DoubleSupplier vX, DoubleSupplier heading) {
+     public DriveCommand(Swerve swerve, DoubleSupplier vY, DoubleSupplier vX, DoubleSupplier heading, XboxController control) {
           this.swerve = swerve;
           this.vX = vX;
           this.vY = vY;
           this.heading = heading;
-          // this.velocity = velocity;
-          // this.control = control;
+          this.control = control;
           controller = swerve.getSwerveController();
 
           addRequirements(swerve);
@@ -48,12 +48,19 @@ public class DriveCommand extends Command {
           }
      }
 
+     private double getLock() {
+          if (control.getBButtonPressed()) {
+               return swerve.getHeading().getRadians();
+          }
+
+          return 0.0;
+     }
+
      @Override
      public void execute() {
           double angle = heading.getAsDouble() * 0.8;
 
           ChassisSpeeds desiredSpeeds = swerve.getTargetSpeeds(vX.getAsDouble(), vY.getAsDouble(), setMax());
-          // Limit velocity to prevent tippy
           Translation2d translation = SwerveController.getTranslation2d(desiredSpeeds);
           translation = SwerveMath.limitVelocity(translation, swerve.getFieldVelocity(), swerve.getPose(),
                     Dimensoes.LOOP_TIME, Dimensoes.ROBOT_MASS, List.of(Dimensoes.CHASSIS),
@@ -62,6 +69,11 @@ public class DriveCommand extends Command {
           SmartDashboard.putString("Translation", translation.toString());
           SmartDashboard.putNumber("VelocityMax", setMax());
           double rotation = controller.config.maxAngularVelocity * angle;
+
+          double output = pid.calculate(swerve.getHeading().getRadians(), getLock());
+          if (control.getLeftTriggerAxis() != 0.3) {
+               swerve.drive(translation, output);
+          }
 
           swerve.drive(translation, rotation);
 
